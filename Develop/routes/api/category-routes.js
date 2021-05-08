1d971bc316242f8390bd0017f91e552bad8c6e91
harrisonblackburn <blackbhr@yahoo.com>
2021-05-07 21:22:51 -0500

    nearly complete with category-routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,15 +3,20 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
-});
+ // find all categories
+  // be sure to include its associated Product
+  router.get('/', async (req, res) => {
+    try {
+      const categoryData = await Category.findAll();
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+// find one category by its `id` value
+// be sure to include its associated Products
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
 
 router.post('/', (req, res) => {
   // create a new category
--- Develop/server.js
+++ Develop/server.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
 require('dotenv').config();
-const Sequelize = require('sequelize');
+const sequelize = require('./config/connection');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+
 // import sequelize connection
 
 const app = express();
@@ -23,6 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  })
 });
