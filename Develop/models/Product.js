086cd119420a846b6b9666bdcca53126c870b8a0
harrisonblackburn <blackbhr@yahoo.com>
2021-05-08 21:07:01 -0500

    comments deleted on models to clean up files

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -8,47 +8,7 @@ class Product extends Model { }
 
 // set up fields and rules for Product model
 
-// `Product`
 
-// * `id`
-
-//   * Integer.
-
-//   * Doesn't allow null values.
-
-//   * Set as primary key.
-
-//   * Uses auto increment.
-
-// * `product_name`
-
-//   * String.
-
-//   * Doesn't allow null values.
-
-// * `price`
-
-//   * Decimal.
-
-//   * Doesn't allow null values.
-
-//   * Validates that the value is a decimal.
-
-// * `stock`
-
-//   * Integer.
-
-//   * Doesn't allow null values.
-
-//   * Set a default value of `10`.
-
-//   * Validates that the value is numeric.
-
-// * `category_id`
-
-//   * Integer.
-
-//   * References the `Category` model's `id`.
 Product.init(
   {
     // define columns
