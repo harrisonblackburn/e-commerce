d1bdb6bf4a581bc79a407a7567016005aac14b5c
harrisonblackburn <blackbhr@yahoo.com>
2021-05-16 21:29:30 -0500

    added association models to index js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -12,6 +12,37 @@ const ProductTag = require('./ProductTag');
 
 // Tags belongToMany Products (through ProductTag)
 
+
+
+// `Product` belongs to `Category`, and `Category` has many `Product` models, as a category can have multiple products but a product can only belong to one category.
+
+// * `Product` belongs to many `Tag` models, and `Tag` belongs to many `Product` models. Allow products to have multiple tags and tags to have many products by using the `ProductTag` through model.
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id', 
+  onDelete: 'CASCADE'
+}); 
+
+Category.hasMany(Product, {
+  onDelete: 'CASCADE'
+});
+
+Category.hasMany(Product, {
+  onDelete: 'CASCADE'
+});
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
+
+
+
 module.exports = {
   Product,
   Category,
